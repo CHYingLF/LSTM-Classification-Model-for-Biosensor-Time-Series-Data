0b802f8230a07d31e33a65ab7324d5c51a0b41bb
Chunhua <66288883+CHYingLF@users.noreply.github.com>
2024-03-10 12:16:03 -0700

    Add files via upload
    Add transformer encoder

--- models/backbone.py
+++ models/backbone.py
@@ -18,15 +18,59 @@ class DNN(nn.ModuleDict):
         for _ in range(self.layer_dim):
             x = self.activation(self.linear(x))
 
-        #print(x.shape)
         out = torch.transpose(x, 1, 2)
         out = self.linear_(out)
         out = torch.transpose(out, 1 ,2)
         out = self.fc(out)
         out = torch.reshape(out, (-1, 2))
         out = self.logsoftmax(out)
-        # print(out.shape)
         return out
+    
+class RNN(nn.Module):
+    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim, dropout_prob):
+        super(LSTMModel, self).__init__()
+
+        self.hidden_dim = hidden_dim
+        self.layer_dim = layer_dim
+
+        self.rnn = nn.RNN(input_dim, hidden_dim, layer_dim, batch_first = True, 
+                            dropout = dropout_prob)
+        
+        self.fc = nn.Linear(hidden_dim, output_dim)
+        self.logsoftmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim, device = x.device).requires_grad_()
+        c0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim, device=x.device).requires_grad_()
+
+        out, (hn, cn) = self.rnn(x, (h0.detach(), c0.detach()))
+        out = self.fc(out[:,-1,:])
+        out1 = self.logsoftmax(out)
+
+        return out1
+    
+class GRU(nn.Module):
+    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim, dropout_prob):
+        super(LSTMModel, self).__init__()
+
+        self.hidden_dim = hidden_dim
+        self.layer_dim = layer_dim
+
+        self.gru = nn.GRU(input_dim, hidden_dim, layer_dim, batch_first = True, 
+                            dropout = dropout_prob)
+        
+        self.fc = nn.Linear(hidden_dim, output_dim)
+        self.logsoftmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim, device = x.device).requires_grad_()
+        c0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim, device=x.device).requires_grad_()
+
+        out, (hn, cn) = self.gru(x, (h0.detach(), c0.detach()))
+        out = self.fc(out[:,-1,:])
+        out1 = self.logsoftmax(out)
+
+        return out1
 
 class LSTMModel(nn.Module):
     def __init__(self, input_dim, hidden_dim, layer_dim, output_dim, dropout_prob):
@@ -42,22 +86,57 @@ class LSTMModel(nn.Module):
         self.logsoftmax = nn.LogSoftmax(dim=1)
 
     def forward(self, x):
-        #print('x:',x)
         h0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim, device = x.device).requires_grad_()
         c0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim, device=x.device).requires_grad_()
 
         out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
         out = self.fc(out[:,-1,:])
         out1 = self.logsoftmax(out)
-        # print(out1.shape)
-        # sys.exit()
+        return out1
+    
+class LSTM_Transformer(nn.Module):
+    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim, dropout_prob):
+        super(LSTMModel, self).__init__()
 
+        self.hidden_dim = hidden_dim
+        self.layer_dim = layer_dim
+
+        self.lstm = nn.LSTM(input_dim, hidden_dim, layer_dim, batch_first = True, 
+                            dropout = dropout_prob)
+        
+        encoder_layer = nn.TransformerEncoderLayer(d_model=256, nhead=12)
+        self.encoder = nn.TransformerEncoder(encoder_layer, num_layers=8)
+        
+        decoer_layer = nn.TransformerDecoderLayer(d_model = 128, n_head = 8)
+        self.decoder = nn.TransformerDecoder(decoder_layer=decoer_layer, num_layers = 12)
+
+        self.fc = nn.Linear(hidden_dim, output_dim)
+        self.logsoftmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, x):
+        # Use LSTM first to extract important info for each vector in the sequence
+        h0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim, device = x.device).requires_grad_()
+        c0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim, device=x.device).requires_grad_()
+        # Last layer hidden out stored in lstm structure
+        out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
+
+        # Attention mechanism to assign weights to each hidden vector, and calculate sequence contribution
+        attention_vector = self.encoder(out)
+        out = attention_vector * out
+        # Finally, decoder layer to decode encoder output
+        out = self.decoder(out)
+
+        out = self.fc(out[:,-1,:])
+        out1 = self.logsoftmax(out)
         return out1
         
 def get_model(model, model_params):
     models = {
+        "dnn": DNN,
+        "gru": GRU,
+        "rnn": RNN,
         'lstm':LSTMModel,
-        "dnn": DNN
+        "lstm_transformer": LSTM_Transformer
     }
 
     return models.get(model.lower())(**model_params)
