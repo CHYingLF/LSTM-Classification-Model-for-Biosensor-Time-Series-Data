2a3574a32eff1b5586df8860a04009d264c9bbcc
Chunhua <66288883+CHYingLF@users.noreply.github.com>
2024-03-10 20:34:57 -0700

    Add files via upload

--- models/backbone.py
+++ models/backbone.py
@@ -100,14 +100,18 @@ class LSTM_Transformer(nn.Module):
 
         self.hidden_dim = hidden_dim
         self.layer_dim = layer_dim
-
+        self.batchNorm = nn.BatchNorm1d(num_features=input_dim)
         self.lstm = nn.LSTM(input_dim, hidden_dim, layer_dim, batch_first = True, 
                             dropout = dropout_prob)
         
-        encoder_layer = nn.TransformerEncoderLayer(d_model=256, nhead=12)
+        encoder_layer = nn.TransformerEncoderLayer(d_model=hidden_dim, nhead=12, 
+                                                   dim_feedforward= 512, dropout=dropout_prob,
+                                                   batch_first= True)
         self.encoder = nn.TransformerEncoder(encoder_layer, num_layers=8)
         
-        decoer_layer = nn.TransformerDecoderLayer(d_model = 128, n_head = 8)
+        decoer_layer = nn.TransformerDecoderLayer(d_model = hidden_dim, n_head = 8,
+                                                  dim_feedforward=512, dropout = dropout_prob,
+                                                  batch_first= True)
         self.decoder = nn.TransformerDecoder(decoder_layer=decoer_layer, num_layers = 12)
 
         self.fc = nn.Linear(hidden_dim, output_dim)
@@ -115,16 +119,18 @@ class LSTM_Transformer(nn.Module):
 
     def forward(self, x):
         # Use LSTM first to extract important info for each vector in the sequence
+        x = self.batchNorm(x)
+
         h0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim, device = x.device).requires_grad_()
         c0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim, device=x.device).requires_grad_()
         # Last layer hidden out stored in lstm structure
         out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
 
         # Attention mechanism to assign weights to each hidden vector, and calculate sequence contribution
-        attention_vector = self.encoder(out)
-        out = attention_vector * out
+        encoder_out = self.encoder(out)
         # Finally, decoder layer to decode encoder output
-        out = self.decoder(out)
+        # x = target, output from encoder layer
+        out = self.decoder(x, encoder_out)
 
         out = self.fc(out[:,-1,:])
         out1 = self.logsoftmax(out)
